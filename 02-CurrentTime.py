bba9d51f277b633524bc0bdcd81db5b94ac0a79f
Kyle Feng <kylezete@gmail.com>
2017-09-19 00:06:43 +0100

    solution for 02-CurrentTime

---
+++ 02-CurrentTime.py
@@ -0,0 +1,8 @@
+# print current date and time
+
+# get solutions from website: https://www.cyberciti.biz/faq/howto-get-current-date-time-in-python/
+# This website provide varies of formats to display current date and time
+
+import time
+print(time.strftime('%c'))
+# %c stands for Appropriate date and time representation
