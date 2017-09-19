815141c5de699ab6e1e443634fa1de00a27982a7
Kyle Feng <kylezete@gmail.com>
2017-09-19 19:10:55 +0100

    solution for 08-MergeListAndSort

---
+++ 08-MergeListAndSort.py
@@ -0,0 +1,11 @@
+# merges two sorted lists into a new sorted list.
+
+# assume two sorted list are:
+list1 = [1,4,6]
+list2 = [2,3,5]
+# first merge these two list
+list = list1+list2
+# Then sort the new list
+# python provide methods for sorting, website:
+#   https://docs.python.org/2/howto/sorting.html
+print(sorted(list))
