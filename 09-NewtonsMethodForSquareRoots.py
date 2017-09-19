8f8956e3d4a052e32ec6a5dda1cd0b2c94940f1e
Kyle Feng <kylezete@gmail.com>
2017-09-19 21:22:15 +0100

    solution for 09-NewtonsMethodForSquareRoots

--- 09-NewtonsMethodForSquareRoots.py
+++ 09-NewtonsMethodForSquareRoots.py
@@ -8,11 +8,19 @@ print("math.sqrt("+str(x)+") = "+str(sqrt))
 
 # try loop for 10 times
 # assume z0 = 1
+#   z=1.0
+#   for i in range(1,11):
+#       tmp = z - ((z*z - x) / (2 * z))
+#       print(tmp)
+#       z=tmp
+# by this way, some result is very close to math.sqrt(x)
+# so,change loop to stop when result stopped changing (or only changes by a very small delta)
 z=1.0
-for i in range(1,11):
+while True:
     tmp = z - ((z*z - x) / (2 * z))
+    if(tmp == sqrt or math.fabs(tmp-sqrt)<0.000000000001):
+        break
     print(tmp)
     z=tmp
-
 
 
