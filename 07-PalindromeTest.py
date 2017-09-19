ebf873af3840883031e5ba1d440d6407d0e34bc5
Kyle Feng <kylezete@gmail.com>
2017-09-19 18:58:23 +0100

    update solution 07-PalindromeTest

--- 07-PalindromeTest.py
+++ 07-PalindromeTest.py
@@ -5,11 +5,16 @@
 # type in a string
 string = str(input("input a string: \n"))
 flag = True
-# check is/not a palindrome
-for i in range(0,int(len(string)/2)):
-    if(string[i] != string[int(len(string))-i-1]):
-        flag=False
-if(flag):
-    print("the string:\n"+string+"\nis a palindrome")
+# if the string size is odd, it can not be a palindrome,
+# for efficiency, I do this
+if(len(string)%2 == 0):
+    # check is/not a palindrome
+    for i in range(0,int(len(string)/2)):
+        if(string[i] != string[int(len(string))-i-1]):
+            flag=False
+    if(flag):
+        print("the string:\n"+string+"\nis a palindrome")
+    else:
+        print("the string:\n"+string+"\nis not a palindrome")
 else:
     print("the string:\n"+string+"\nis not a palindrome")
