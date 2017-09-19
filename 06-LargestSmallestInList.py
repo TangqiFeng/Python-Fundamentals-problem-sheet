62d5f57e31ae43da1cfdd707b4e628688da97ff0
Kyle Feng <kylezete@gmail.com>
2017-09-19 16:11:28 +0100

    06-reaf a list

---
+++ 06-LargestSmallestInList.py
@@ -0,0 +1,10 @@
+# get largest and smallest number from a list
+# get help from website https://stackoverflow.com/questions/252703/append-vs-extend
+
+# type in a list, type:int
+list = []
+# set the length of list: 5
+for i in range(0,5):
+    list.extend([input()])
+
+print(list)
