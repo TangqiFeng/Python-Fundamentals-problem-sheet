1bd3a91808fe46171c06b5f8901c0855a520c1b8
Kyle Feng <kylezete@gmail.com>
2017-09-19 19:57:50 +0100

    step 2 for 09

--- 09-NewtonsMethodForSquareRoots.py
+++ 09-NewtonsMethodForSquareRoots.py
@@ -8,7 +8,12 @@ sqrt_2 = math.sqrt(z)
 print("math.sqrt(2) = "+str(sqrt_2))
 
 # now just try x from 1 to 10 using the formular
-for x in range(1,11):
+# the result : 1.25, 1.5, 1.75, 2.0 ------
+# so, x should between 1 and 2
+# I change the loop: x from 1.0 to 2.0, increase by 0.1
+x=1.0
+while(x<=2.0):
     z_next = z - ((z*z - x) / (2 * z))
     print(z_next)
+    x+=0.1
 
