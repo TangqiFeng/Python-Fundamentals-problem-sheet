b72762a0a8499b7045f9180893fe7004563291c5
Kyle Feng <kylezete@gmail.com>
2017-09-19 16:20:59 +0100

    solution for 06-LargestSmallestInList

--- 06-LargestSmallestInList.py
+++ 06-LargestSmallestInList.py
@@ -5,6 +5,13 @@
 list = []
 # set the length of list: 5
 for i in range(0,5):
-    list.extend([input()])
-
-print(list)
+    list.extend([int(input())])
+print("the list: "+str(list))
+max,min = list[0],list[0]
+# compare each element, get the max and min
+for x in list:
+    if(x>max):
+        max = x
+    if(x<min):
+        min = x
+print("largest: "+str(max)+"\nsmallest "+str(min)) 
